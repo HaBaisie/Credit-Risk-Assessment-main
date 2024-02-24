2d4c2e24f61c22e87bbef39347652c17bc6352db
BaisieTech <57533856+HaBaisie@users.noreply.github.com>
2024-02-24 13:28:43 +0100

    Update explore.py

--- explore.py
+++ explore.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pickle as pickle
+import sklearn
 import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
